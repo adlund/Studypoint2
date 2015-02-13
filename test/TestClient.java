9d7f0b0aff7bba635657da0143bbf091182f1859
Alex <Alex@10.50.137.244>
2015-02-13 11:03:04 +0100

    + part 2

--- test/TestClient.java
+++ test/TestClient.java
@@ -11,35 +11,35 @@ import static org.junit.Assert.*;
  * @author Lars Mortensen
  */
 public class TestClient {
-  
-  public TestClient() {
-  }
-  
-  @BeforeClass
-  public static void setUpClass() {
-    new Thread(new Runnable(){
-      @Override
-      public void run() {
-        EchoServer.main(null);
-      }
-    }).start();
-  }
-  
-  @AfterClass
-  public static void tearDownClass() {
-    EchoServer.stopServer();
-  }
-  
-  @Before
-  public void setUp() {
-  }
-  
-  @Test
-  public void send() throws IOException{
-    EchoClient client = new EchoClient();
-    client.connect("localhost",9090);
-    client.send("Hello");
-    assertEquals("HELLO", client.receive());
-  }
-  
+
+    public TestClient() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                EchoServer.main(null);
+            }
+        }).start();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        EchoServer.stopServer();
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void send() throws IOException {
+        EchoClient client = new EchoClient();
+        client.connect("localhost", 9090);
+        client.send("Hello");
+        assertEquals("HELLO", client.receive());
+    }
+
 }
